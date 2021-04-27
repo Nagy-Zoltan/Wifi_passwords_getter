7775c662aa92bd543fe78ac505b5031f28cb41dc
Nagy-Zoltan <47283309+Nagy-Zoltan@users.noreply.github.com>
2021-04-27 23:26:47 +0200

    Add files via upload

---
+++ wifi_pwds.py
@@ -0,0 +1,16 @@
+from subprocess import *
+import re
+
+def get_wifi_passwords():
+    profiles = check_output('netsh wlan show profiles').decode('utf-8', 'backslashreplace')
+    ssids = [res[2:-1] for res in re.findall(': .*\r', profiles)]
+    profile_details = [check_output(f'netsh wlan show profile "{ssid}" key=clear').decode('utf-8', 'backslashreplace') for ssid in ssids]
+    pwd_sections = [re.findall('--+ \r\n.+?\r\n\r\n', pro_det, re.DOTALL)[-2] for pro_det in profile_details]
+    passwords = [re.findall(': .+\r', pwd_section)[-1][2:-1] for pwd_section in pwd_sections]
+    return dict(zip(ssids, passwords))
+
+for key, val in get_wifi_passwords().items():
+    print(f'{key} ---> {val}')
+
+
+input()
